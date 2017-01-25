e0e580a8affc9412d8ccf6025da0cd1d1b794a15
lamari imad <imad.lamari@dauphine.eu>
2017-01-25 18:31:57 +0100

    Ajout projet Animal

---
+++ src/com/animal/model/Animal.java
@@ -0,0 +1,56 @@
+package com.animal.model;
+
+
+
+/**
+ * Write a description of class Animal here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Animal
+{
+    // instance variables - replace the example below with your own
+    private String race;
+    private String nom;
+
+    /**
+     * Constructor for objects of class Animal
+     */
+    public Animal()
+    {
+        this.nom="tom";
+        this.race="chat";
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public String getNom()
+    {
+        return this.nom;
+    }
+    
+    public void setNom(String nom)
+    {
+        this.nom=nom;
+    }
+    public String getRace()
+    {
+        return this.race;
+    }
+    
+    public void setRace(String race)
+    {
+        this.race=race;
+    }
+    public void informationAnimal()
+    {
+        System.out.println (this.nom + " est un " + this.race);
+        
+    }
+}
---
+++ src/com/animal/model/Maitre.java
@@ -0,0 +1,62 @@
+package com.animal.model;
+
+
+/**
+ * Write a description of class Maître here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Maitre
+{
+    // instance variables - replace the example below with your own
+    private Animal animal;
+    String nom;
+
+    /**
+     * Constructor for objects of class Maître
+     */
+    public Maitre()
+    {
+        // initialise instance variables
+        this.nom="TOTO";
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+   /* public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }*/
+    
+    public void setNom( String nom)
+    {
+        this.nom=nom;
+    }
+    
+    public String getNom()
+    {
+        return this.nom;
+    }
+    
+     public void setAnimal( Animal animal)
+    {
+        this.animal=animal;
+    }
+    
+    public Animal getAnimal()
+    {
+        return this.animal;
+    }
+    
+    public String animalDeCompagnie()
+    {
+        return this.nom + " est le maître de " + this.animal.getNom();
+    }
+}
